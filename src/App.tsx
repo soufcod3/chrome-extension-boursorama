3b7d2c2ab56f0122e3e635b06df69b9741db117f
Soufiane <soufiane.ait@outlook.fr>
2023-04-08 12:57:20 +0200

    display json in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({})
+
+  const analyze = useCallback(() => {
+    chrome.tabs && chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, (tabs) => {
+      console.log('TABS', tabs)
+      chrome.tabs.sendMessage(
+        // Current tab id
+        tabs[0].id || 0,
+
+        // Message type
+        { type: 'GET_DOM' },
+
+        // Callback executed when the content script sends a response
+        (response: any) => {
+          console.log('RES', response)
+          setData(response)
+        });
+    });
+  }, [])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => analyze()}>Analyser</button>
+        {Object.keys(data).length > 0 &&
+          <pre style={{ width: '100%', textAlign: 'left' }}>{JSON.stringify(data, undefined, 2)}</pre>
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
